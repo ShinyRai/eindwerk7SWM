ea2dbd4e75ebdad5e8a22ee7965523472117b5ef
Nele Van Nevel <vanneveln@visocloud.org>
2018-03-26 10:20:56 +0200

    klaarmaken voor database aan index te koppelen. om producten aan te passen

--- js/dist/main.js
+++ js/dist/main.js
@@ -724,17 +724,14 @@ var Modal = function ($) {
 //# sourceMappingURL=modal.js.map
 $('#video').modal()
 
+$(".roze").on('mouseover', function() {
+	$(".fles_hover").attr("src","images/flespink.png");
+});
 
-$(document).ready(function(){
-    $(".roze").on('mouseover', function() {
-    	$(".fles_hover").attr("src","images/flespink.png");
-    });
+$(".geel").on('mouseover', function() {
+	$(".fles_hover").attr("src","images/flesyello.png");
+});
 
-   $(".geel").on('mouseover', function() {
-    	$(".fles_hover").attr("src","images/flesyello.png");
-    });
-
-   $(".groen").on('mouseover', function() {
-    	$(".fles_hover").attr("src","images/flesgreen.png");
-    });
+$(".groen").on('mouseover', function() {
+	$(".fles_hover").attr("src","images/flesgreen.png");
 });
