b066d842d91497c1dcc95c9debd4b035618e1240
Nele Van Nevel <vanneveln@visocloud.org>
2018-03-27 14:57:20 +0200

    no message

--- wijzigen.php
+++ wijzigen.php
@@ -1,5 +1,5 @@
 <?php
-if (empty($_POST["dvd_id"])){
+if (empty($_POST["id"])){
 	// als de url-parameter niet werd meegegeven ga terug naar admin.php
 	header("Location:admin.php");
 	exit;
@@ -16,24 +16,19 @@ include("includes/db_conn.php");
 
 // SQL-injectie voorkomen
 	// 1) zet integers om met (int) $_POST['naamveld']
-	$_POST['dvd_id'] = (int) $_POST['dvd_id'];
-	$_POST['waardering'] = (int) $_POST['waardering'];
+	$_POST['id'] = (int) $_POST['id'];
+	$_POST['prijs'] = (int) $_POST['prijs'];
 	
 	// 2) met mysqli_real_escape_string($conn, $_POST['naamveld']
 	$_POST['naam'] = mysqli_real_escape_string($conn, $_POST['naam']);
-	$_POST['acteurs'] = mysqli_real_escape_string($conn, $_POST['acteurs']);
-	$_POST['regisseur'] = mysqli_real_escape_string($conn, $_POST['regisseur']);
-	$_POST['categorie'] = mysqli_real_escape_string($conn, $_POST['categorie']);
-	$_POST['prijs'] = mysqli_real_escape_string($conn, $_POST['prijs']);
-	$_POST['jaartal'] = mysqli_real_escape_string($conn, $_POST['jaartal']);
-	$_POST['duur'] = mysqli_real_escape_string($conn, $_POST['duur']);
-	$_POST['inhoud'] = mysqli_real_escape_string($conn, $_POST['inhoud']);
-	$_POST['foto'] = mysqli_real_escape_string($conn, $_POST['foto']);
+	$_POST['beschrijving'] = mysqli_real_escape_string($conn, $_POST['acteurs']);
+	$_POST['prijs'] = mysqli_real_escape_string($conn, $_POST['regisseur']);
+	$_POST['afb'] = mysqli_real_escape_string($conn, $_POST['categorie']);
 
 
 // stap 2: De query opstellen en uitvoeren
 
-$query = "UPDATE dvd SET naam = '{$_POST['naam']}', acteurs = '{$_POST['acteurs']}', regisseur = '{$_POST['regisseur']}', categorie = '{$_POST['categorie']}', prijs = '{$_POST['prijs']}', jaartal = '{$_POST['jaartal']}', duur = '{$_POST['duur']}', inhoud = '{$_POST['inhoud']}', waardering = {$_POST['waardering']}, foto = '{$_POST['foto']}' WHERE dvd_id = {$_POST['dvd_id']}";
+$query = "UPDATE producten SET naam = '{$_POST['naam']}', beschrijving = '{$_POST['beschrijving']}', prijs = '{$_POST['prijs']}', afb = '{$_POST['afb']}' WHERE id = {$_POST['id']}";
 
 if (!$result = mysqli_query($db,$query)) {
     echo "FOUT: Query kon niet uitgevoerd worden"; 
