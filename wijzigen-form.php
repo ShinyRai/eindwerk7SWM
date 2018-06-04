5c5a103b860565449c7e4b2e154dd34519a927d6
Nele Van Nevel <vanneveln@visocloud.org>
2018-06-04 11:00:38 +0200

    no message

---
+++ wijzigen-form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>wijzigen</title>
+</head>
+<body>
+
+</body>
+</html>
+
+	<form action="<?php echo $_SERVER['PHP_SELF']."?id=".$_GET['id']; ?>" method="post">
+
+			<label for="naam">Naam product:</label>
+			<input type="text" name="naam" id="naam" autofocus required value="<?php echo $rij['naam'];?>">
+			<br>
+			<label for="beschrijving">Beschrijving product:</label>
+			<input type="text" name="naam" id="naam" autofocus required value="<?php echo $rij['beschrijving'];?>">
+			<br>
+			<label for="prijs">Prijs product:</label>
+			<input type="number" name="prijs" id="prijs" required value="<?php echo $rij['prijs'];?>">
+			<br><br>
+			
+
+			<input type="submit" name="submit" id="submit" value="submit">
+			
+		</form>
+
+	</body>
+	</html>
